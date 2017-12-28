7b0fbc16f8a25d1b2c08f1f57a775901e812d513
Katherine Rust <katherine.rust.andrews@gmail.com>
2017-12-28 02:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function dec(n) {
   return n
 }
 
-function makeInt(){
-  return parseInt(a, 10)
+function makeInt(n){
+  return parseInt(n, 10)
 }
 
-function preserveDecimal(a) {
-  return parseFloat(a)
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
